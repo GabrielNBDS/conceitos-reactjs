3383777f90fa742fed4e58d9374e4563b4ded17e
Gabriel Nascimento <gabriel.nbds@gmail.com>
2020-04-07 22:56:37 -0400

    Mission Complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+    const repository = await api.post("/repositories", {
+      title: `titulo ${Date.now()}`,
+      url: "aaa",
+      techs: ["bbb", "ccc"]
+    })
+
+    setRepositories([...repositories, repository.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    
+    await api.delete(`/repositories/${id}`);
+
+    setRepositories(repositories.filter(repository => repository.id !== id));
   }
+
+  useEffect( () => {
+    api.get("/repositories").then(response => setRepositories(response.data)); 
+  }, [])
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+      {repositories.map(repository => (
+        <li key={repository.id}>
+          {repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+
+      ))}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
